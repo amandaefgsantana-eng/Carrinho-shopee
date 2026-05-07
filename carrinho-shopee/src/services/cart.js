898e92b7e9d955f8d7b6a61f70b9daf88394f5cf
amandaefgsantana-eng <amandaefg.santana@gmail.com>
2026-05-07 12:42:32 -0300

    Update cart.js

--- carrinho-shopee/src/services/cart.js
+++ carrinho-shopee/src/services/cart.js
@@ -1,58 +1,64 @@
-//quais açoes meu carrinho pode fazer
+//🌸 quais ações meu carrinho pode fazer 🌸
 
-//CASOS DE USO
-// ✅ -> adicionar item no carrinho
+// 🛍️ CASOS DE USO
+
+// 💖 -> adicionar item no carrinho
 async function addItem(userCart, item) {
   userCart.push(item);
 }
 
-// ✅ -> calcular o total do carrinho
+// 💖 -> calcular o total do carrinho
 async function calculateTotal(userCart) {
-  console.log("\nShopee Cart TOTAL IS:");
+  console.log("\n💗 Shopee Cart TOTAL 💗");
 
   const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
-  console.log(`🎁Total: ${result}`);
+
+  console.log(`🎀 Total: R$ ${result}`);
 }
 
-// -> deletar item do carrinho
+// 💖 -> deletar item do carrinho
 async function deleteItem(userCart, name) {
   const index = userCart.findIndex((item) => item.name === name);
 
   if (index !== -1) {
     userCart.splice(index, 1);
+    console.log("🩷 Item removido com sucesso!");
   }
 }
 
-// -> ✅ remover um item - diminui um item
+// 💖 -> remover apenas 1 unidade do item
 async function removeItem(userCart, item) {
-  //1. encontrar o indice do item
+  //1️⃣ encontrar o índice do item
   const indexFound = userCart.findIndex((p) => p.name === item.name);
 
-  //2. Caso não encontre o item
+  //2️⃣ caso não encontre o item
   if (indexFound == -1) {
-    console.log("item não encontrado");
+    console.log("🌷 Item não encontrado");
     return;
   }
 
-  //3. item > 1 subtrair um item
+  //3️⃣ se quantidade > 1, diminui um item
   if (userCart[indexFound].quantity > 1) {
     userCart[indexFound].quantity -= 1;
+    console.log("💞 Quantidade diminuída!");
     return;
   }
 
-  //4. caso item = 1 deletar o item
+  //4️⃣ se quantidade = 1, remove o item
   if (userCart[indexFound].quantity == 1) {
     userCart.splice(indexFound, 1);
+    console.log("🎀 Item removido do carrinho!");
     return;
   }
 }
 
-// ✅ mostra todos os items do carrinho
+// 💖 -> mostrar todos os itens do carrinho
 async function displaycart(userCart) {
-  console.log("\nShopee cart list:");
+  console.log("\n🩷 Shopee Cart List 🩷");
+
   userCart.forEach((item, index) => {
     console.log(
-      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+      `🌸 ${index + 1}. ${item.name} - R$ ${item.price} | ${
         item.quantity
       }x | Subtotal = ${item.subtotal()}`
     );
